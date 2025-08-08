54ddf66f534e507da7bfe6019bb6d92575e51e28
Siya Sonpatki <135365124+ssonpatki@users.noreply.github.com>
2025-08-08 11:02:47 -0700

    Added files from shared repo to personal repo

---
+++ GA2-CS325-main/README.md
@@ -0,0 +1,2 @@
+# GA2-CS325
+Second group assignment
---
+++ GA2-CS325-main/assignment2.py
@@ -0,0 +1,22 @@
+'''
+    This file contains the template for Assignment1.  For testing it, I will place it
+    in a different directory, call the function <min_attendance_for_long_weekend>, and check its output.
+    So, you can add/remove  whatever you want to/from this file.  But, don't change the name
+    of the file or the name/signature of the following function.
+
+    Also, I will use <python3> to run this code.
+'''
+
+def min_attendance_for_long_weekend(input_file_path, output_file_path):
+    '''
+        This function will contain your code.  It wil read from the file <input_file_path>,
+        and will write its output to the file <output_file_path>.
+    '''
+    pass
+
+'''
+    To test your function, you can uncomment the following command with the the input/output
+    files paths that you want to read from/write to.  Do NOT forget to comment it out before
+    submitting.
+'''
+# min_attendance_for_long_weekend('', '')
